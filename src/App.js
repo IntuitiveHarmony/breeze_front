1211f5c02a9e1a03eb2ac8a5e526c59b2d69933c
ununique3 <j.horst77@gmail.com>
2022-09-22 18:01:11 -0600

    adding tempo, main volume, track tolume

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import {useState} from 'react';
 import { Counter } from './features/counter/Counter';
 import './App.css';
 import axios from 'axios'
+import { Song, Track, Instrument, Effect } from 'reactronica';
 import * as Tone from 'tone'
 import Test2 from './components/Test2'
 import Synth from './components/Synth'
@@ -9,6 +10,9 @@ import MonoSynth from './components/MonoSynth'
 
 function App() {
   const [sequences, setSequences] = useState([])
+  const [isPlaying, setIsPLaying] = useState(false)
+  const [tempo, setTempo] = useState(100)
+  const [volume, setVolume] = useState()
 
   //---------------------------------------------
   //  GET SEQUENCE ROUTE
@@ -23,13 +27,31 @@ function App() {
    .catch((error) => console.error(error))
   }
 
+
+  const handleVolume = (e) => {
+    setVolume(e.target.value)
+  }
+  const handleTempo = (e) => {
+    setTempo(e.target.value)
+  }
+
+  const play = () => {
+    setIsPLaying(!isPlaying)
+  }
   return (
     <>
       <h1>Breeze</h1>
       <h4></h4>
-      <Test2 />
+    {/*  <Test2 /> */}
+    <Song isPlaying={isPlaying} volume={volume} bpm={tempo}>
       <Synth />
       <MonoSynth />
+    </Song>
+      {isPlaying ? <button onClick={() => play()}>Stop</button> : <button onClick={() => play()}>Play</button> }
+      <label>Main volume</label>
+      <input onChange={handleVolume} type='range' step='1' min='-100' max='0' ></input>
+      <label>Tempo: {tempo}bpm</label>
+      <input onChange={handleTempo} type='range' step='1' min='10' max='700' ></input>
     </>
   );
 }
--- src/components/MonoSynth.js
+++ src/components/MonoSynth.js
@@ -1,25 +1,44 @@
 import {useState} from 'react';
-import { Song, Track, Instrument } from 'reactronica';
+import { Song, Track, Instrument, Effect } from 'reactronica';
 
 
 
 const MonoSynth = () => {
-  const [steps, setSteps] = useState(['C3', 'E3', null, 'E3','C3', 'E3', 'B2', 'E3'])
-  const [isPlaying, setIsPLaying] = useState(false)
+  // const [monoGrid, setMonoGrid] = useState({
+  //   steps: ['C3', 'E3', null, 'E3','C3', 'E3', 'B2', 'E3'],
+  //   delay: [  0 ,   0 ,    0,    0,   0,    0,    0,   0 ]
+  // })
+  const [steps, setSteps] = useState([
+    {name: 'C3'}, {name: 'E3'}, {name: null}, {name: 'E3'}, {name: 'C3'}, {name: 'E3'}, {name: 'B2'}, {name: 'E3'}
+  ])
+  const [delayWet, setDelayWet] = useState(0)
+  const [monoVolume, setMonoVolume] = useState()
 
 
-  const play = () => {
-    setIsPLaying(!isPlaying)
+
+  const handleDelayFeedback = (e) => {
+    setDelayWet(e.target.value)
   }
+  const handleMonoVolume = (e) => {
+    setMonoVolume(e.target.value)
+  }
+
 
   return (
     <>
-    <Song isPlaying={isPlaying}>
-      <Track steps={steps}>
+    <hr/>
+
+      <Track steps={steps} volume={monoVolume} onStepPlay={(step, index) => {
+          console.log(step, index);
+        }}>
         <Instrument type='synth' />
+        <Effect type='feedbackDelay' wet={delayWet} />
       </Track>
-      <button onClick={() => play()}>Play</button>
-    </Song>
+
+      <label>Mono Delay: </label>
+    <input onChange={handleDelayFeedback} type='range' step='.1' min='0' max='.9' ></input>
+    <label>Mono Volume: </label>
+  <input onChange={handleMonoVolume} type='range' step='1' min='-100' max='0' ></input>
     </>
   )
 }
