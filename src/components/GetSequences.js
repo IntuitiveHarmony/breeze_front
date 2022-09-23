6cc42fd27b1aeaf066c749bf357309775760f854
ununique3 <j.horst77@gmail.com>
2022-09-23 17:12:17 -0600

    learning redux

--- src/components/GetSequences.js
+++ src/components/GetSequences.js
@@ -1,16 +1,26 @@
 import { Component } from 'react'
 import axios from 'axios'
 
+
+//using https://www.youtube.com/watch?v=4uzEUATtNHQ&ab_channel=TheNetNinja for inspiration on this
+//LifeCyle Hooks, class based componets...
 class GetSequences extends Component {
-  componentDidMount(){
+  state = {
+    sequences: [ ]
+  }
+  componentDidMount() {
     axios.get('https://breeze-back.herokuapp.com/api/sequences')
       .then(res => {
         console.log(res.data)
+        this.setState({
+          post: res.data
+        })
       })
   }
   render(){
     return (
-      <h3>Hai</h3>
+      <>
+      </>
     )
   }
 
